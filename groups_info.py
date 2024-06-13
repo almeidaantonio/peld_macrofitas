e7d1f4538462f707d63b43c673ae5d1f5506cb88
almeidaantonio <almeida.antonio.dev@gmail.com>
2024-06-13 16:48:48 -0300

    Formalizado grupos restantes das legendas

---
+++ groups_info.py
@@ -0,0 +1,20 @@
+samples = {
+    1: "Fevereiro/Março",
+    2: "Maio/Junho",
+    3: "Agosto/Setembro",
+    4: "Novembro/Dezembro",
+}
+
+enviroments = {
+    "LGAR": "Lagoa das Garças",
+    "LGUA": "Lagoa Guaraná",
+    "LPAT": "Lagoa dos Patos",
+    "LOSM": "Lagoa Osmar",
+    "LFEC": "Lagoa Fechada",
+    "LVEN": "Lagoa Ventura",
+}
+
+env_types = {
+    "C": "Lagoa Aberta (com comunicação ao rio principal)",
+    "I": "Lagoa Fechada (sem comunicação ao rio principal)",
+}
